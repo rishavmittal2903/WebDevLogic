fbe655fc920a53edc8ab4f9d7654fe5a6770f060
unknown <rishavmittal2903@gmail.com>
2020-06-25 04:50:36 +0530

    Updates

---
+++ precache-manifest.0afbbdb16e2329813361ebe83b115259.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd6e15871dc09a0ae7966bd59570e3b7",
+    "url": "/WebDevLogic/index.html"
+  },
+  {
+    "revision": "b99b855360f31e527378",
+    "url": "/WebDevLogic/static/css/main.65d67ea0.chunk.css"
+  },
+  {
+    "revision": "6fc9d66f04c4d18a2bfe",
+    "url": "/WebDevLogic/static/js/2.5925cd83.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/WebDevLogic/static/js/2.5925cd83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b99b855360f31e527378",
+    "url": "/WebDevLogic/static/js/main.5e0924ff.chunk.js"
+  },
+  {
+    "revision": "a0bba75080dcac942a6a",
+    "url": "/WebDevLogic/static/js/runtime-main.68afd555.js"
+  },
+  {
+    "revision": "34a686d91e9c8b157e5d17f375ab425e",
+    "url": "/WebDevLogic/static/media/Calisto-MT-Bold.34a686d9.ttf"
+  },
+  {
+    "revision": "74abf7eb7351857ed68f7c9fe9216c21",
+    "url": "/WebDevLogic/static/media/Calisto-MT-Italic.74abf7eb.ttf"
+  },
+  {
+    "revision": "583e9c844f5d3cc9c7f3d408275356ff",
+    "url": "/WebDevLogic/static/media/Calisto-MT.583e9c84.ttf"
+  },
+  {
+    "revision": "a437db440af91825a818bd10af86b20a",
+    "url": "/WebDevLogic/static/media/about-me-bckg.a437db44.jpg"
+  },
+  {
+    "revision": "76a008515b98b0da922e77a68a8d6e58",
+    "url": "/WebDevLogic/static/media/aboutme.76a00851.jpg"
+  },
+  {
+    "revision": "ad9fcf833a1e00e2e346263e230574e2",
+    "url": "/WebDevLogic/static/media/banner.ad9fcf83.jpg"
+  },
+  {
+    "revision": "54c350720de9ec6c7d75023a099b05a5",
+    "url": "/WebDevLogic/static/media/book-bckg.54c35072.jpg"
+  },
+  {
+    "revision": "48c9ad2748e1d46614057a2af4798776",
+    "url": "/WebDevLogic/static/media/doc.48c9ad27.png"
+  },
+  {
+    "revision": "90ce921e16cd0f087fde196a03caca59",
+    "url": "/WebDevLogic/static/media/double-line.90ce921e.svg"
+  },
+  {
+    "revision": "251dbe331fcf016665087c9a9fe6a3a1",
+    "url": "/WebDevLogic/static/media/medi.251dbe33.jpg"
+  },
+  {
+    "revision": "a76698ae1575674eab2ca4f8df4ff319",
+    "url": "/WebDevLogic/static/media/quotes-bckg.a76698ae.jpg"
+  },
+  {
+    "revision": "a2e387d99f8ca6ba92a34a6d2ead1af5",
+    "url": "/WebDevLogic/static/media/security.a2e387d9.jpg"
+  },
+  {
+    "revision": "59c0cf8fe6dc252d432b702918872729",
+    "url": "/WebDevLogic/static/media/social-bckg.59c0cf8f.jpg"
+  },
+  {
+    "revision": "3573a577401e1ff60d6a2a4435298931",
+    "url": "/WebDevLogic/static/media/time.3573a577.jpg"
+  },
+  {
+    "revision": "51e16fab05907f5ac0fb86cd19d11286",
+    "url": "/WebDevLogic/static/media/top-banner-bckg.51e16fab.jpg"
+  }
+]);
